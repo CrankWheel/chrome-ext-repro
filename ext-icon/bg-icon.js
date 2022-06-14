163c608e6d682533a3771bce4e8a52d82989b022
Jói Sigurdsson <joi@crankwheel.com>
2022-06-14 14:29:34 +0000

    Set up so alarm triggers window which sends message which triggers bg page wakeup and icon update

--- ext-icon/bg-icon.js
+++ ext-icon/bg-icon.js
@@ -1,33 +1,29 @@
-var redIcon = false;
-var count = Math.floor(Math.random() * 10000);
-
 function updateStuff() {
-    redIcon = !redIcon;
-    count += 1;
-    chrome.browserAction.setBadgeText({text: "" + count});
-    var iconFile = "x19.png";
-    if (redIcon) iconFile = "cwicon19red.png";
-    chrome.browserAction.setIcon({path: iconFile});
-
-    // When loaded unpacked we can do less than one minute. This is timed
-    // to be right after the background page unloads.
-    chrome.alarms.clearAll();
-    chrome.alarms.create({when: Date.now() + 15300});
-    //setTimeout(updateStuff, 100);
+    chrome.storage.local.get({redIcon: false, count: 0}, function (stg) {
+        var redIcon = !stg.redIcon;
+        var count = stg.count + 1;
+        chrome.browserAction.setBadgeText({text: "" + count});
+        var iconFile = "x19.png";
+        if (redIcon) iconFile = "cwicon19red.png";
+        chrome.browserAction.setIcon({path: iconFile});
+        chrome.storage.local.set({redIcon, count});
+        chrome.alarms.clearAll();
+        chrome.alarms.create({when: Date.now() + 15300});
+    });
 }
+
+chrome.alarms.onAlarm.addListener(createIconSwitchingPage);
 
 chrome.browserAction.onClicked.addListener(function () {
     updateStuff();
-
-    //createIconSwitchingPageIfNotCreated();
-
-    //chrome.alarms.create({periodInMinutes: 1});
 });
 
-chrome.alarms.onAlarm.addListener(updateStuff);
+chrome.runtime.onMessage.addListener(function (msg) {
+    updateStuff();
+});
 
-function createIconSwitchingPageIfNotCreated() {
+function createIconSwitchingPage() {
     if (chrome.extension.getViews().length == 1) {
-        chrome.tabs.create({url: "creator.html"});
+        chrome.windows.create({url: "creator.html", state: "minimized"});
     }
 }
--- ext-icon/creator.js
+++ ext-icon/creator.js
@@ -1,28 +1,6 @@
-var continueDoingStuff = true;
-
-function doStuffOnTimeout() {
-    stuff();
-    if (continueDoingStuff) setTimeout(doStuffOnTimeout, 10);
+function doStuff() {
+    chrome.runtime.sendMessage({msg: "hello world"});
+    window.close();
 }
 
-doStuffOnTimeout();
-
-var redIcon = false;
-var count = 0;
-
-function stuff() {
-    redIcon = !redIcon;
-    count += 1;
-    chrome.browserAction.setBadgeText({text: "" + count});
-    var iconFile = "x19.png";
-    if (redIcon) iconFile = "cwicon19red.png";
-    chrome.browserAction.setIcon({path: iconFile});
-}
-
-function clickHandler() {
-    continueDoingStuff = !continueDoingStuff;
-    if (continueDoingStuff) {
-        doStuffOnTimeout();
-    }
-}
-document.getElementById('toggle-btn').addEventListener('click', clickHandler);
+doStuff();
