6c78e535db600aa77a928d84a1e4eda6696517db
Jói Sigurdsson <joi@crankwheel.com>
2022-06-16 21:42:47 +0000

    With rdevlin@'s changes to bg-icon.js

--- ext-icon/bg-icon.js
+++ ext-icon/bg-icon.js
@@ -1,30 +1,43 @@
 var redIcon = false;
-var count = Math.floor(Math.random() * 10000);
 
 function updateStuff() {
     redIcon = !redIcon;
-    count += 1;
-    chrome.browserAction.setBadgeText({text: "" + count});
+    let stamp = Date.now();
+    console.warn(`Updating stuff: ${stamp}`);
+    let now = new Date(stamp);
+    chrome.browserAction.setBadgeText({text: `${now.getHours()}:${now.getMinutes()}`}, () => { console.warn(`setBadgeText done: ${stamp}`); });
     var iconFile = "x19.png";
     if (redIcon) iconFile = "cwicon19red.png";
-    chrome.browserAction.setIcon({path: iconFile});
+    console.warn(`setting icon: ${stamp}`)
+    chrome.browserAction.setIcon({path: iconFile}, () => { console.warn(`set icon done: ${stamp}`); });
 
     // When loaded unpacked we can do less than one minute. This is timed
     // to be right after the background page unloads.
-    chrome.alarms.clearAll();
-    chrome.alarms.create({when: Date.now() + 15300});
+    console.warn(`clearning alarms: ${stamp}`);
+    chrome.alarms.clearAll(() => { console.warn(`clear alarms done: ${stamp}`); });
+
+    chrome.alarms.create({when: Date.now() + 17000});
+
+    console.warn(`updateStuff() done: ${stamp}`);
+
     //setTimeout(updateStuff, 100);
 }
 
 chrome.browserAction.onClicked.addListener(function () {
+    console.warn('BrowserAction Clicked');
     updateStuff();
+    console.warn('BrowserAction Updated');
 
     //createIconSwitchingPageIfNotCreated();
 
     //chrome.alarms.create({periodInMinutes: 1});
 });
 
-chrome.alarms.onAlarm.addListener(updateStuff);
+chrome.alarms.onAlarm.addListener(() => {
+  console.warn('Alarm triggered');
+  updateStuff();
+  console.warn('Alarm update done');
+});
 
 function createIconSwitchingPageIfNotCreated() {
     if (chrome.extension.getViews().length == 1) {
